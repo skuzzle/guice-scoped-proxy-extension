21fe98f8166bbb43f97e515a8abb0876d1bf4bc5
Simon Taddiken <simon.taddiken@gmail.com>
2019-01-25 07:12:49 +0100

    java9

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,8 @@ module de.skuzzle.inject.proxy {
     exports de.skuzzle.inject.proxy;
 
     requires cglib;
-    requires guava;
-    requires transitive guice;
+    requires com.google.guice;
+    requires transitive com.google.common;
     requires javax.inject;
     requires org.objenesis;
 }
