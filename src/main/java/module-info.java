7ae59ff63db5abe9fecdd7beb836e4b3e15eb086
Simon Taddiken <simon.taddiken@gmail.com>
2019-01-25 18:13:52 +0100

    transitive require

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,7 @@ module de.skuzzle.inject.proxy {
     opens de.skuzzle.inject.proxy to cglib;
 
     requires org.objenesis;
-    requires com.google.guice;
+    requires transitive com.google.guice;
     requires com.google.common;
 
     requires cglib;
